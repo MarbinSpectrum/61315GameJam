c676fe17901402583342b31e5a9e5455d85dc957
MarbinSpectrum <9507ym@naver.com>
2025-02-14 21:57:49 +0900

    fixed mat

--- Assets/@Scripts/Object/Eater.cs
+++ Assets/@Scripts/Object/Eater.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Eater : MonoBehaviour
 {
     // --------------------------------------------------
     // Components
     // --------------------------------------------------
-    [SerializeField] private Animation animation;
-    [SerializeField] private GameObject objectEmoji;
+    [SerializeField] private GameObject angryEmoji;
+    [SerializeField] private GameObject owoEmoji;
+    [SerializeField] private GameObject[] chObj;
     [SerializeField] private GameObject[] table;
-    
+
     // --------------------------------------------------
     // Variables
     // --------------------------------------------------
     private BlockData _data;
-    
+    private GameObject nowChObj;
+
     // --------------------------------------------------
     // Functions
     // --------------------------------------------------
@@ -24,15 +27,25 @@ public class Eater : MonoBehaviour
         var y = data.row;
         transform.position = new Vector3(x, y, 0);
 
+        angryEmoji.gameObject.SetActive(false);
+        owoEmoji.gameObject.SetActive(false);
+        
+        foreach (var obj in chObj)
+            obj.SetActive(false);
+
+        int randomIdx = Random.Range(0, chObj.Length);
+        nowChObj = chObj[randomIdx];
+        nowChObj.SetActive(true);
+
         foreach (var obj in table)
             obj.SetActive(false);
 
         if (x == 0)
         {
             transform.rotation = Quaternion.Euler(0, 0, -90);
-            if(y == 1)
+            if (y == 1)
                 table[2].SetActive(true);
-            else   if(y == n)
+            else if (y == n)
                 table[0].SetActive(true);
             else
                 table[1].SetActive(true);
@@ -40,9 +53,9 @@ public class Eater : MonoBehaviour
         else if (x == m + 1)
         {
             transform.rotation = Quaternion.Euler(0, 0, +90);
-            if(y == 1)
+            if (y == 1)
                 table[0].SetActive(true);
-            else   if(y == n)
+            else if (y == n)
                 table[2].SetActive(true);
             else
                 table[1].SetActive(true);
@@ -50,9 +63,9 @@ public class Eater : MonoBehaviour
         else if (y == 0)
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
-            if(x == 1)
+            if (x == 1)
                 table[0].SetActive(true);
-            else   if(x == m)
+            else if (x == m)
                 table[2].SetActive(true);
             else
                 table[1].SetActive(true);
@@ -60,25 +73,35 @@ public class Eater : MonoBehaviour
         else if (y == n + 1)
         {
             transform.rotation = Quaternion.Euler(0, 0, 180);
-            if(x == 1)
+            if (x == 1)
                 table[2].SetActive(true);
-            else   if(x == m)
+            else if (x == m)
                 table[0].SetActive(true);
             else
                 table[1].SetActive(true);
-            
+
         }
-        
+
 
     }
-    
+
     public void OnEat()
     {
         // TODO : 와그작 애니메이션 재생
+        nowChObj.GetComponent<Animation>().Play("Eat");
     }
 
     public void OnAngry()
     {
         // TODO : 화나는 이모지 연출
+        angryEmoji.gameObject.SetActive(true);
+        angryEmoji.GetComponent<ParticleSystem>().Play();
     }
+
+    public void OnNice()
+    {
+        owoEmoji.gameObject.SetActive(true);
+        owoEmoji.GetComponent<ParticleSystem>().Play();
+    }
+
 }
