0981574f48bcd854cc430da3c44a4cd2655ab1e2
MarbinSpectrum <9507ym@naver.com>
2025-02-14 17:59:58 +0900

    feat : add gamemanager

--- Assets/@Scripts/Manager/Managers.cs
+++ Assets/@Scripts/Manager/Managers.cs
@@ -4,9 +4,12 @@ public class Managers : SingletonBase<Managers>
     private readonly ToastManager _toastManager = new();
     private readonly DataManager _dataManager = new();
     private readonly MapManager _mapManager = new();
+    private readonly GameManager _gameManager = new();
     
     public static UIManager UI => Instance._uiManager;
     public static ToastManager Toast => Instance._toastManager;
     public static DataManager Data => Instance._dataManager;
     public static MapManager Map => Instance._mapManager;
+    public static GameManager Game => Instance._gameManager;
+
 }
