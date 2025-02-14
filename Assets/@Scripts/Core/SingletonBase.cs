1a2639f8a1e5d7b2f5c106fe23274f6deaaefea7
JeongJongMun <garlic@consalad.io>
2025-02-14 15:22:59 +0900

    feat: SingletonBase 추가

---
+++ Assets/@Scripts/Core/SingletonBase.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class SingletonBase<T> where T : class, new()
+{
+    private static T instance = null;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new T();
+
+            return instance;
+        }
+    }
+    public static void Init(Action doneCallback = null)
+    {
+        if (instance == null)
+            instance = new T();
+        
+        doneCallback?.Invoke();
+    }
+}
